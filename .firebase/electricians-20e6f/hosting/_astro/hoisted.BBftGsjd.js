e18ef1318a8981ad362810f71daf7e09eed7ba2c
Victor <vhpc24@proton.me>
2024-09-06 16:00:39 -0500

    Ran npm run build

---
+++ .firebase/electricians-20e6f/hosting/_astro/hoisted.BBftGsjd.js
@@ -0,0 +1,1 @@
+const e=document.querySelector(".hamburger-menu"),t=document.querySelector(".hidden-menu");e.addEventListener("click",()=>{e.classList.toggle("active"),t.classList.toggle("active")});
